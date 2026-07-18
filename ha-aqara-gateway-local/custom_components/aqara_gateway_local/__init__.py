ff62277b1870bf13ce75bf6ae60ba2c63ca60d89
lieut-gkhn <77892101+lieut-gkhn@users.noreply.github.com>
2026-07-18 20:25:40 +0300

    Update __init__.py

--- ha-aqara-gateway-local/custom_components/aqara_gateway_local/__init__.py
+++ ha-aqara-gateway-local/custom_components/aqara_gateway_local/__init__.py
@@ -1,1 +1,8 @@
+from homeassistant.core import HomeAssistant
 
+from .const import DOMAIN
+
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    hass.data.setdefault(DOMAIN, {})
+    return True
